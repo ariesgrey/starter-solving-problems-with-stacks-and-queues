f3f549b69e9524cde41f720230f838f014c15a29
aries grey <ariesgrey88@gmail.com>
2023-10-06 22:15:54 -0500

    connected() completed, all tests passing

--- src/connected_friends/connected_friends.js
+++ src/connected_friends/connected_friends.js
@@ -1,5 +1,46 @@
 const Queue = require("../lib/queue");
 
-const connected = (G, s, r) => {};
+const connected = (graph, startUser, endUser) => {
+	const users = Object.keys(graph);
+
+	// If 'graph' is empty (has no keys), return false
+	if (users.length === 0) {
+		return false;
+	}
+	// If 'startUser' is equal to 'endUser', return true
+	if (startUser === endUser) {
+		return true;
+	}
+
+	// Initialize a new array 'enqueued' that contains 'startUser'
+	const enqueued = [startUser];
+	// Initialize a new empty queue 'discovered'
+	const discovered = new Queue();
+	// Enqueue 'startUser'
+	discovered.enqueue(startUser);
+
+	// While 'discovered' isn't empty...
+	while (discovered.first) {
+		// Dequeue a value from 'discovered' and name it 'user'
+		const user = discovered.dequeue();
+		// For each friend 'followedUser' in 'graph[user]'...
+		const following = graph[user];
+		for (const followedUser of following) {
+			// If 'followedUser' is equal to 'endUser', return true
+			if (followedUser === endUser) {
+				return true;
+			}
+			// If 'enqueued' doesn't include 'followedUser'...
+			if (!enqueued.includes(followedUser)) {
+				// Add 'followedUser' to 'enqueued'
+				enqueued.push(followedUser);
+				// Enqueue 'followedUser' to 'discovered'
+				discovered.enqueue(followedUser);
+			}
+		}
+	}
+	// Return false
+	return false;
+};
 
 module.exports = connected;
